493887b9e686d0e9220a7cd7cac5768c4326dce3
fission6 <fission6@fission6.(none)>
2011-10-01 16:42:01 -0400

    initial commit

---
+++ indeed/indeed.py
@@ -0,0 +1,47 @@
+import urllib2
+import json
+from urllib import urlencode
+
+
+class IndeedApi(object):
+    
+    def __init__(self, publisher_id, **kwargs):
+    
+        self.publisher_id = publisher_id
+        self.base_url = 'http://api.indeed.com/ads/'
+
+    def search(self, query):
+        
+        action = 'apisearch'
+        query_params = {
+            'q' : query,
+            'format' : 'json',
+            'publisher' : self.publisher_id
+        }
+        
+        query_string = urlencode(query_params)
+        service_req = '{0}{1}?{2}'.format(self.base_url, action, query_string)
+        
+        request = urllib2.urlopen(service_req)
+        results = request.read()
+        data = json.loads(results)
+        
+        return data
+    
+    def job_details(self, job_keys):
+        
+        action = 'apigetjobs'
+        query_params = {
+            'q' : query,
+            'format' : 'json',
+            'publisher' : self.publisher_id
+        }
+        
+        query_string = urlencode(query_params)
+        service_req = '{0}{1}?{2}'.format(self.base_url, action, query_string)
+        
+        request = urllib2.urlopen(service_req)
+        results = request.read()
+        data = json.loads(results)
+        
+        return data
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(name="python-indeed",
+    version=".1",
+    packages=["indeed"],
+    description="small wrapper for indeed.com api",
+    author="none",
+    author_email = "none",
+    license="",
+    url="",
+    long_description="",
+    platforms=["any"],
+    classifiers=[],
+)
